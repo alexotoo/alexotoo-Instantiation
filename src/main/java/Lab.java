da9c3e7a4ff5a4cbfe8ade70c2afe72ee4588fbe
Abraham Alex Otoo <alexotoous@outlook.com>
2023-03-04 06:46:07 +0000

    object Instantiated

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        String read =new String();
+        return read;
     }
 }
